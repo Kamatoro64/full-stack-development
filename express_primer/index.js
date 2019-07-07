164fc843300c43a5cad96025b6fd31c2c6794db0
NG Chester <zhentingchester.ng@murex.com>
2019-07-07 20:04:15 +0800

    Making use of the express-handlebars module (template engine) to render html

--- express_primer/index.js
+++ express_primer/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
 
 const app = express();
 
-
+// express-handlebars middleware - see: https://www.npmjs.com/package/express-handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Body parser middleware
 app.use(express.json());
@@ -13,6 +16,10 @@ app.use(express.urlencoded({ extended: false })); // For form submissions
 // Logger middlware (Interestingly this relies on the body parser so it has to come after!)
 app.use(logger);
 
+// Create a route
+app.get('/', (req, res) => {
+	res.render('index');
+})
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
