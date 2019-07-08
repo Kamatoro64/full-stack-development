b59f269035347b0ee9f5d7eef39c4a48b36d55a4
NG Chester <zhentingchester.ng@murex.com>
2019-07-08 12:21:04 +0800

    Adding a route so that we don't get an error when visitng the main page

--- meanauthapp/app.js
+++ meanauthapp/app.js
@@ -7,8 +7,12 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const port = 3000
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Invalid Endpoint')
+});
 
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
-})
+});
