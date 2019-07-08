43d25866a716c9d0283ecd2af772017e42eab6a9
NG Chester <zhentingchester.ng@murex.com>
2019-07-08 12:51:15 +0800

    Initialise the cors middleware

--- meanauthapp/app.js
+++ meanauthapp/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path'); // Core module no explicit installation required
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors'); // https://www.npmjs.com/package/cors basically helps us achieve https://enable-cors.org/server_expressjs.html
 const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
 
 const port = 3000;
+
+// cors middleware
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Invalid Endpoint')
