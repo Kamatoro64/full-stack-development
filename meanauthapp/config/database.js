a64e9afc30caf1c77ffd19e7b12b995a0f7e2930
NG Chester <zhentingchester.ng@murex.com>
2019-07-09 10:19:45 +0800

    Changing DB Connection string to connect to MongoDB Atlas, bugged due to corporate network restrictions, to test outside

---
+++ meanauthapp/config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+	database: 'mongodb+srv://kamatoro64:kamatoro64@kamatoro64cluster-zbuzc.mongodb.net/test?retryWrites=true&w=majority',
+	secret: 'kamatoro64'
+}
+
+
+// Use Mongodb Atlas to create a
